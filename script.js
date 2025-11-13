05fbf5a786da6eda28e1f33cc65412197954b161
Raj Chaurasiya <rc808129@gmail.com>
2025-11-13 15:04:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll('.code')
+
+inputs[0].focus();
+
+
+//forward typing
+inputs.forEach((input, index)=>{
+	//handle the input event
+	input.addEventListener('input', (e)=>{
+		const value = e.target.value;
+
+		//only allow one digit
+		if(value.length>1){
+			e.target.value = value.slice(0,1)
+		}
+
+		if(value.length===1 && index<inputs.length-1){
+			inputs[index+1].focus();
+		}
+	})
+
+	//handling keydown for backspace
+	input.addEventListener('keydown', (e)=>{
+		if(e.key=== 'Backspace'){
+			if(e.target.value === '' && index>0){
+				e.preventDefault()
+				inputs[index-1].focus();
+				
+			}else{
+				e.target.value="";
+			}
+		}
+	})
+
+
+	
+
+})
